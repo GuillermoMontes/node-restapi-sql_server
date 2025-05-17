6afff0f42ece8ff2a7fbf2e7d7ec55357aa6c985
Gmontes <ghmontes92@gmail.com>
2025-05-17 17:55:22 -0300

    Agrego web.config para Azure

--- server/app.js
+++ server/app.js
@@ -16,11 +16,16 @@ app.set("port", config.port)
 
 app.use(express.json())
 
-app.use(cors({
-  origin: '*', 
+
+
+const corsOptions = {
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"]
-}));
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 
 
 app.use(taskRutes); 
