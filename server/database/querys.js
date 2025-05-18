ddccfe285041ca0b0e7bb3ddb25016d079636825
Gmontes <ghmontes92@gmail.com>
2025-05-18 18:24:50 -0300

    arreglo delete task error

--- server/database/querys.js
+++ server/database/querys.js
@@ -2,6 +2,6 @@ export default {
     getAllTalks : "SELECT * FROM allTasks",
     createTalk : "INSERT INTO allTasks (title,description) VALUES (@title, @description)",
     getTalkById: "SELECT * FROM allTasks Where Id = @id",
-    deleteTalk: "DELETE FROM [tasks].[dbo].[allTasks] WHERE Id = @Id",
+    deleteTalk: "DELETE FROM allTasks WHERE Id = @Id",
     actualizarTask: "UPDATE allTasks SET title = @title, description = @description WHERE id = @id" 
 }
